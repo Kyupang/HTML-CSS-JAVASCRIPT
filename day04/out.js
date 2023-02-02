0cf149c65b5164054d6d41ee36dae5d1bfd8c2bb
KimKyuHwan <vkdls754@gmail.com>
2023-02-02 14:56:57 +0900

    commit

---
+++ day04/out.js
@@ -0,0 +1,5 @@
+let a = function(n) {
+    for(i=0;i<n; i++){
+        console.log(i);
+    }
+}
---
+++ day04/out2.js
@@ -0,0 +1,6 @@
+function 여행하다(){
+    document.write("여행 잘하세요~ <br>")
+}
+function 쉬다(){
+    document.write("잘 쉬세요 <br>")
+}
